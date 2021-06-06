b4d08c7da08a1bbb933871d0581c762a54a6ffe5
Taras Valakh <58398502+mmeerrccyy@users.noreply.github.com>
2021-06-06 20:26:13 +0300

    added slug validation

--- api_server/views.py
+++ api_server/views.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.generic.base import View
+from shop.models import Category, Product
 
 
 # Create your views here.
@@ -15,3 +16,13 @@ class UsernameValidation(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'username already exists!'}, status=400)
         return JsonResponse({'username_valid': True})
+
+
+class SlugValidation(View):
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        slug = data['slug']
+        if Category.objects.filter(slug=slug).exists() or Product.objects.filter(slug=slug).exists():
+            return JsonResponse({'slug_error': 'slug already exists!'}, status=400)
+        return JsonResponse({'slug_valid': True})
--- static/js/UsernameValidator.js
+++ static/js/UsernameValidator.js
@@ -1,18 +1,37 @@
 const usernameField = document.getElementById('id_username');
 
-usernameField.addEventListener("keyup", (e) => {
+usernameField.addEventListener("change", (e) => {
     const usernameValue = e.target.value;
 
     usernameField.classList.remove('is-invalid');
 
-    if(usernameValue.length > 0){
+    if (usernameValue.length > 0) {
         fetch('/api/validate-username/', {
             body: JSON.stringify({
                 username: usernameValue
             }),
             method: "POST",
         }).then(res => res.json()).then(data => {
-            if(data.username_error){
+            if (data.username_error) {
+                usernameField.classList.add('is-invalid')
+            }
+        })
+    }
+});
+
+usernameField.addEventListener("keyup", (e) => {
+    const usernameValue = e.target.value;
+
+    usernameField.classList.remove('is-invalid');
+
+    if (usernameValue.length > 0) {
+        fetch('/api/validate-username/', {
+            body: JSON.stringify({
+                username: usernameValue
+            }),
+            method: "POST",
+        }).then(res => res.json()).then(data => {
+            if (data.username_error) {
                 usernameField.classList.add('is-invalid')
             }
         })
